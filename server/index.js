afa122cb3d77b732044a62aa4a40f3f4b0e2c795
Manthan Patel <manthan.patel@cygnetinfotech.com>
2021-07-27 02:00:06 +0530

    Server Dotenv

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+require('dotenv').config()
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api/todos', require('./routes/todos'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
